11eea74169df845555826efa567772caed7409f1
Matias Nahuel Ierace <matias.ierace@gmail.com>
2023-11-12 12:01:00 -0300

    primera aproximacion a niveles OK

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const usersRouter = require ('./routes/users.router.js')
 const productsRouter = require ('./routes/products.router.js')
 const errorHandler = require ('./utils/errors.js')
-const { addLogger } = require ('./utils/logger.js')
+const { Logger } = require ('./utils/logger.js')
 
 const app = express()
 const PORT = 8080
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use("/mockingusers", usersRouter.router)
 app.use("/mockingproducts", productsRouter.router)
 app.use(errorHandler)
-app.use (addLogger)
+app.use (Logger)
 
 app.get('/', (req, res) => {
     res.send({message: "Prueba de Logger!"})
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,24 +1,50 @@
 const winston = require ('winston')
 
+const customLevelOptions = {
+    levels: {
+        fatal: 0,
+        error: 1,
+        warning: 2,
+        info: 3,
+        debug: 4,
+    },
+    colors: {
+        fatal: 'red',
+        error: 'orange',
+        warning: 'yellow',
+        info: 'blue',
+        debug: 'white',
+    }
+}
 const logger = winston.createLogger({
     /**
      * A partir de winston.createLogger creamos nuestro logger con los trasportes que 
      * necesitamos, en este caso, definimos un transporte de consola para funcionar
      * sólo a partir del nivel http.
      * */
-    transports: [
-        new winston.transports.Console({ level: "http"})
+    levels: customLevelOptions.levels, // Ahora los niveles se basarán en los definidos
+    transports: [ 
+        new winston.transports.Console({ 
+            level: "info", // El nivel debe coincidir con nuestra nueva configuracion
+            /**
+             * El formato sólo definirá el que se muestren los mensajes de otra forma
+             * además de colocar los colores que nos interesan
+            **/
+           format: winston.format.combine(
+            winston.format.colorize({ colors: customLevelOptions.colors }),
+            winston.format.simple()
+           )
+        }),
+        new winston.transports.File({ 
+            filename:'./errors.log', 
+            level:'warning', // Esta vez no es "warn", sino "warning" según la nueva configuración
+            format: winston.format.simple()
+        })
     ]
 })
-    /**
-     * Ahora, a partir de un middleware, vamos a colocar en el objeto req el logger,
-     * aprovechamos además para hacer nuestro primer log.
-    **/
-
-    const addLogger = (req, res, next) => {
-        req.logger = logger
-        req.logger.http(`${req.method} en ${req.url} - ${new Date().toLocaleTimeString()}`)
+const Logger = (req, res, next) => {
+        logger.info(`${req.method} en ${req.url} - ${new Date().toLocaleTimeString()}`)
         next()
-    }
+}
 
-    module.exports = { addLogger }
+module.exports = { Logger }
